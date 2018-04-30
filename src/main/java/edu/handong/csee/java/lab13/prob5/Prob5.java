ab39564af9a8b69d93d208c689ba9e28a6557b76
User <User@172.17.158.223>
2018-04-30 22:00:09 +0900

    add comment

--- src/main/java/edu/handong/csee/java/lab13/prob5/Prob5.java
+++ src/main/java/edu/handong/csee/java/lab13/prob5/Prob5.java
@@ -7,8 +7,8 @@ public class Prob5 {
 		int count = 0; // variable to save count of difference
 		if (m1.length != m2.length) 
 			return false;			// if the number of row is different, return false
-		for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[i].length; j++)
+		for (int i = 0; i < m1.length; i++) {		// each row
+            for (int j = 0; j < m1[i].length; j++)  // each column
             {
                 if (m1[i][j] != m2[i][j])
                 	count++; 	// if value is not same count + 1
@@ -37,25 +37,25 @@ public class Prob5 {
 		int[][] m2 = new int[n1][n2]; // create array
 		
 		System.out.print("Enter List1: "); // print
-		for (int i = 0; i < m1.length; i++)
-			for (int j = 0; j < m1[i].length; j++)
+		for (int i = 0; i < m1.length; i++)  // each row
+			for (int j = 0; j < m1[i].length; j++) // each column
 				m1[i][j] = keyboard.nextInt(); // get integer from keyboard
 		
 		System.out.print("Enter list2: "); // print
-		 for (int i = 0; i < m2.length; i++){
-		    for (int j = 0; j < m2[i].length; j++)
+		 for (int i = 0; i < m2.length; i++){  // each row
+		    for (int j = 0; j < m2[i].length; j++) // each column
 		                m2[i][j] = keyboard.nextInt(); 	// get integer from keyboard
 		 
-		    for (i = 0; i < m1.length; i++){
-		    	for (int j = 0; j < m1[i].length; j++)
+		    for (i = 0; i < m1.length; i++){ // each row
+		    	for (int j = 0; j < m1[i].length; j++) // each column
 		    		System.out.print(m1[i][j] + " "); // print array's values
 		    	System.out.println(); // line change to show m1.length
 		    }
 		    
 		 System.out.println(); // line change
 		 
-		 for (i = 0; i < m2.length; i++){
-		    for (int j = 0; j < m2[i].length; j++)
+		 for (i = 0; i < m2.length; i++){  // each row
+		    for (int j = 0; j < m2[i].length; j++) // each column
 		      System.out.print(m2[i][j] + " "); // print array's values
 		    System.out.println(); // line change to show m2.length
 		 }
